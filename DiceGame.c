58029197fb77509f164d77e2dc41730292f9340f
s1280039 <s1280039@u-aizu.ac.jp>
2021-07-03 18:08:02 +0900

    Won/lost message added

--- DiceGame.c
+++ DiceGame.c
@@ -12,5 +12,13 @@ int main(void) {
 	printf("Die 2: %d\n", d2);
 	printf("Total value: %d\n", d1 + d2);
 
+	if (d1 + d2 > 7) {
+		printf("You won!\n");
+	}
+	else 
+	{
+		printf("You lost!\n");
+	}
+
 	return 0;
 }
