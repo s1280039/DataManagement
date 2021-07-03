118a1a3fc543c3a2925a942501e15feb9aa88019
s1280039 <s1280039@u-aizu.ac.jp>
2021-07-03 17:54:08 +0900

    Greetings added

--- DiceGame.c
+++ DiceGame.c
@@ -6,8 +6,13 @@ int main(void) {
 	srand((unsigned int)time(NULL));
 	int d1 = rand() % 6 + 1;
 	int d2 = rand() % 6 + 1;
+	char s[256];
 
-	printf("\nRolling the dice...\n");
+	printf("\nWhat is your name?\n> ");
+	scanf("%s", s);
+	printf("Hello, %s!\n\n");
+
+	printf("Rolling the dice...\n");
 	printf("Die 1: %d\n", d1);
 	printf("Die 2: %d\n", d2);
 	printf("Total value: %d\n", d1 + d2);
